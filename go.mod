1ed1e8a313bdcc362ee9a6bd5b6b17a84a0fd197
David Blooman <david.blooman@gmail.com>
2019-05-28 15:36:28 +0100

    cleanup

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/depop/terraform-provider-logentries
 go 1.12
 
 require (
-	github.com/depop/logentries v0.0.0-20180530131341-ecb331a4a2b0
+	github.com/depop/logentries v0.0.0-20190528141714-cf541324c208
 	github.com/hashicorp/terraform v0.12.0
 )
--- logentries/data_source_logentries_logset.go
+++ logentries/data_source_logentries_logset.go
@@ -1,6 +1,9 @@
 package logentries
 
 import (
+	"errors"
+
+	logentries "github.com/depop/logentries"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -18,5 +21,26 @@ func dataSourceLogentriesLogSet() *schema.Resource {
 }
 
 func dataSourceLogentriesLogSetRead(d *schema.ResourceData, meta interface{}) error {
-	return resourceLogentriesLogSetRead(d, meta)
+
+	client := meta.(*logentries.Client)
+	res, err := client.LogSets.Read(&logentries.LogSetsReadRequest{})
+	if err != nil {
+		panic(err)
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, logset := range res.LogSets {
+		if logset.Name == d.Get("name").(string) {
+			d.SetId(logset.ID)
+		}
+	}
+
+	if d.Get("Id") == "" {
+		return errors.New("logset does not exist")
+	}
+
+	return nil
+
 }
